c409d0dba029ce295e3bae20f83f9382957c4e37
Iustin Dumitrescu <iustin.dumitrescu@nxp.com>
2018-03-09 15:34:44 +0000

    sja1105: Remove user space reset mechanism
    Remove user space reset mechanism. It is now handled in the driver.
    
    Issue: ALB-2268
    Signed-off-by: Iustin Dumitrescu <iustin.dumitrescu@nxp.com>

--- recipes-kernel/sja1105/files/sja1105.sh
+++ recipes-kernel/sja1105/files/sja1105.sh
@@ -13,15 +13,3 @@
 
 OPT=""
 lsmod | grep -q "^sja1105pqrs"  > /dev/null || insmod /lib/modules/`uname -r`/kernel/drivers/spi/sja1105pqrs.ko ${OPT}
-
-restart_sja()
-{
-        # Force a cold reset of the first SJA.
-        # See SJA1105PQRS User Manual Rev. 0.03  Chapter 6.2 Reset Generation Unit
-        echo 'w:100440:deadbeef' > /sys/kernel/debug/sja1105p-0/general/register_rw
-
-        rmmod sja1105pqrs
-        insmod /lib/modules/`uname -r`/kernel/drivers/spi/sja1105pqrs.ko ${OPT}
-}
-
-mount | grep -q " on / type nfs " > /dev/null || restart_sja

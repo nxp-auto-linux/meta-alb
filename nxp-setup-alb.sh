8de48d7c529894b65a5cba2bbe5d62bf29eea535
Ionut Vicovan <Ionut.Vicovan@nxp.com>
2020-10-22 18:27:33 +0300

    nxp-setup-alb.sh: Fix layer list and program validation
    - script validation (size check) was done against an invalid path
    - generated layers list was wrong. meta-alb was before LS layers,
    which made that bbappends in meta-alb were overridden by bbappends
    from meta-qoriq
    
    Issue: ALB-5819
    
    Signed-off-by: Ionut Vicovan <Ionut.Vicovan@nxp.com>

--- nxp-setup-alb.sh
+++ nxp-setup-alb.sh
@@ -80,9 +80,10 @@ else
 fi
 
 SOURCESDIR="sources"
+ALBROOTDIR=${ROOTDIR}/${SOURCESDIR}/meta-alb
 
 # Validate by size that this file is (the same as) the original one in meta-alb
-ORIGINALFILE="`find \"$ROOTDIR/$SOURCESDIR\" -name $PROGNAME`"
+ORIGINALFILE="`find \"$ALBROOTDIR\" -name $PROGNAME`"
 if [ -e "$ORIGINALFILE" ]; then
 
     PROGSIZE=`ls -l "$ROOTDIR/$PROGNAME" | cut -d' ' -f 5`
@@ -108,7 +109,6 @@ if [ -e ${ROOTDIR}/${SOURCESDIR}/oe-core ]; then
     OEROOTDIR=${ROOTDIR}/${SOURCESDIR}/oe-core
 fi
 FSLROOTDIR=${ROOTDIR}/${SOURCESDIR}/meta-freescale
-ALBROOTDIR=${ROOTDIR}/${SOURCESDIR}/meta-alb
 PROJECT_DIR=${ROOTDIR}/build_${MACHINE}
 
 prompt_message () {
@@ -252,6 +252,16 @@ do
 done
 OPTIND=$OLD_OPTIND
 
+ALB_LAYER_LIST=" \
+    meta-alb \
+    meta-alb-dev \
+    meta-aa-integration \
+    meta-vnp \
+    meta-gvip \
+    \
+    $extra_layers \
+"
+
 LAYER_LIST=" \
     meta-openembedded/meta-oe \
     meta-openembedded/meta-multimedia \
@@ -266,14 +276,6 @@ LAYER_LIST=" \
     meta-linaro/meta-optee \
     \
     meta-freescale \
-    \
-    meta-alb \
-    meta-alb-dev \
-    meta-aa-integration \
-    meta-vnp \
-    meta-gvip \
-    \
-    $extra_layers \
 "
 
 LSLAYERS=" \
@@ -292,8 +294,14 @@ if [ $? -eq 0 ]; then
         meta-linaro/meta-linaro-toolchain \
     "
 
-    add_layers_for_machines "${S32V234LAYERS}" "${S32V234MACHINE}"
     add_layers_for_machines "${LSLAYERS}" "${BBLSMACHINE}"
+
+    # ALB layers after LSLAYERS, to make sure ALB .bbappends are applied last
+    LAYER_LIST="$LAYER_LIST \
+    $ALB_LAYER_LIST \
+    "
+
+    add_layers_for_machines "${S32V234LAYERS}" "${S32V234MACHINE}"
 
 fi
  

456ba5d3e74904902774c0110d8462b6ecfed48f
Radu Pirea (NXP OSS) <radu-nicolae.pirea@oss.nxp.com>
2021-10-22 12:29:28 +0300

    s32v234campp: add all the interfaces into one brdige
    Add all the switches interfaces into one bridge.
    Remove sw1-p4. The port is connected to nothing.
    
    Issue: ALB-7996
    
    Signed-off-by: Andrei Botila <andrei.botila@nxp.com>
    Signed-off-by: Radu Pirea (NXP OSS) <radu-nicolae.pirea@oss.nxp.com>

--- recipes-core/init-net/files/init-ip-link-s32v234campp.sh
+++ recipes-core/init-net/files/init-ip-link-s32v234campp.sh
@@ -7,26 +7,20 @@ STATUS=`cat /proc/device-tree/soc/aips-bus@40000000/spi@40057000/status 2> /dev/
 if [ "x$STATUS" = "xokay" ]; then
 	echo "Initializing bridge/switch interfaces"
 	ip link add dev br0 type bridge vlan_filtering 1
-	ip link add dev br1 type bridge vlan_filtering 1
 	ip link set dev eth0 up
-	ip link set dev sw0-p0 master br0
 	ip link set dev sw0-p1 master br0
 	ip link set dev sw0-p3 master br0
 	ip link set dev sw0-p4 master br0
-	ip link set dev sw1-p1 master br1
-	ip link set dev sw1-p2 master br1
-	ip link set dev sw1-p3 master br1
-	ip link set dev sw1-p4 master br1
+	ip link set dev sw1-p1 master br0
+	ip link set dev sw1-p2 master br0
+	ip link set dev sw1-p3 master br0
 	ip link set dev br0 up
-	ip link set dev br1 up
-	ip link set dev sw0-p0 up
 	ip link set dev sw0-p1 up
 	ip link set dev sw0-p3 up
 	ip link set dev sw0-p4 up
 	ip link set dev sw1-p1 up
 	ip link set dev sw1-p2 up
 	ip link set dev sw1-p3 up
-	ip link set dev sw1-p4 up
 
 else
 	echo "Not initializing bridge/switch interfaces"

1862888687ca40ceff6fa6824ed1d21da226052c
Andrei Cherechesu <andrei.cherechesu@nxp.com>
2022-04-15 19:42:09 +0300

    host-prepare: Add python3-dev package
    Add the "python3-dev" package ("python3-devel" for non-Debian-based
    distros) to the host-prepare.sh script, since it is needed as
    a HOSTTOOL for building U-Boot.
    
    Issue: ALB-8753
    Signed-off-by: Andrei Cherechesu <andrei.cherechesu@nxp.com>

--- scripts/host-prepare-ubuntu-mint-debian.sh
+++ scripts/host-prepare-ubuntu-mint-debian.sh
@@ -8,7 +8,7 @@ if test $force_update; then UPDATE_FLAG='-y --force-yes';fi
 # https://www.yoctoproject.org/docs/2.4.2/ref-manual/ref-manual.html#intro-requirements
 PKGS=" curl gawk wget git-core diffstat unzip texinfo \
      build-essential chrpath socat cpio python python3 python3-pip python3-pexpect \
-     xz-utils debianutils iputils-ping \
+     python3-dev xz-utils debianutils iputils-ping \
      libsdl1.2-dev xterm libncurses5-dev"
 # pkgs required for fsl use
 PKGS="$PKGS make vim-common tofrodos libstring-crc32-perl screen parted \

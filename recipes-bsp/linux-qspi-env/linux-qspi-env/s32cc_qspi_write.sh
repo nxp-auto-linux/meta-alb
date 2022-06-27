fd9989484b08c7e8180680bc9c032d78bdf43ea5
Ciprian Marian Costea <ciprianmarian.costea@nxp.com>
2022-06-27 11:21:20 +0300

    linux-qspi-env: Write padding in QSPI Memory if needed
    Issue: ALB-8984
    Signed-off-by: Ciprian Marian Costea <ciprianmarian.costea@nxp.com>

--- recipes-bsp/linux-qspi-env/linux-qspi-env/s32cc_qspi_write.sh
+++ recipes-bsp/linux-qspi-env/linux-qspi-env/s32cc_qspi_write.sh
@@ -12,7 +12,10 @@ if [ $# -ne 2 ]; then
 	exit 1
 fi
 
-FILESIZE=$(stat -c %s "$2" 2>/dev/null)
+qspi_offset="$1"
+file_to_write="$2"
+
+FILESIZE=$(stat -c %s "$file_to_write" 2>/dev/null)
 if [ $? -ne 0 ]; then
 	echo "Failed to get size on specified file"
 	exit 1
@@ -33,12 +36,29 @@ if [ $? -ne 0 ]; then
 	exit 1
 fi
 
-if ! flash_erase "$FLASH_INTERFACE" "$1" "$SECTORS_TO_ERASE"; then
-	echo "flash_erase command failed with error: $?"
+if ! flash_erase "$FLASH_INTERFACE" "$qspi_offset" "$SECTORS_TO_ERASE"; then
+	echo "flash_erase command failed with error: "$?""
 	exit 1
 fi
 
-if ! mtd_debug write "$FLASH_INTERFACE" "$1" "$FILESIZE" "$2"; then
-	echo "mtd_debug write command failed with error: $?"
+if ! mtd_debug write "$FLASH_INTERFACE" "$qspi_offset" "$FILESIZE" "$file_to_write"; then
+	echo "mtd_debug write command failed with error: "$?""
 	exit 1
+fi
+
+padding_size=$((SECTORS_TO_ERASE * ERASE_SECTOR_SIZE - FILESIZE))
+if [ $? -ne 0 ]; then
+	echo "Failed to compute 'padding_size'"
+	exit 1
+fi
+if [ $padding_size -gt 0 ]; then
+	padding_offset=$((qspi_offset + FILESIZE))
+	if [ $? -ne 0 ]; then
+		echo "Failed to compute 'padding_offset'"
+		exit 1
+	fi
+	if ! mtd_debug write "$FLASH_INTERFACE" "$padding_offset" "$padding_size" /dev/zero; then
+		echo "mtd_debug write for padding command failed with error: "$?""
+		exit 1
+	fi
 fi

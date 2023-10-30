6b9a537c5514877ad6dcef443d15fa0b795599be
erhan cengiz <erhan9398ua@gmail.com>
2023-10-30 14:27:11 +0300

    Update host-prepare-ubuntu-mint-debian.sh
    lz4 installation package is changed to liblz4-tool to fix error at host_prepare.sh

--- scripts/host-prepare-ubuntu-mint-debian.sh
+++ scripts/host-prepare-ubuntu-mint-debian.sh
@@ -9,7 +9,7 @@ if test $force_update; then UPDATE_FLAG='-y --force-yes';fi
 PKGS=" curl gawk wget git-core diffstat unzip texinfo \
      build-essential chrpath socat cpio python2 python3 python3-pip python3-pexpect \
      xz-utils debianutils iputils-ping \
-     libsdl1.2-dev xterm libncurses5-dev lz4 zstd"
+     libsdl1.2-dev xterm libncurses5-dev liblz4-tool zstd"
 # pkgs required for fsl use
 PKGS="$PKGS make vim-common tofrodos libstring-crc32-perl screen parted \
      dosfstools mtools udev"
